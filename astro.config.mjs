4228bc443b743447e866a5b1b9206b24eea9d5c9
Lourdes Juarez <ljp@pepo.local>
2024-01-22 22:18:41 +0100

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site:'https://lourdesjupo.github.io/',
-  base:'/PF_Astro',
+  base:'/PortFolio',
   outDir: 'docs',
   build: {
     assets: 'assets',
