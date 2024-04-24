c5499a7fd76d4fb192a03653c3e01da9d8a2e7be
Lourdesjupo <lourdes.jupo@gmail.com>
2024-04-24 13:08:57 +0200

    Add CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site:'https://lourdesjupo.github.io/',
-  outDir: 'docs',
+  outDir: 'dist',
   build: {
     assets: 'assets',
   }
